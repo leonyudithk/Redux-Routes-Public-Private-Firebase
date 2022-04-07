400fdee316e3d40a4be9b22e52c0d0c1958c4a03
yudithkleon <leonyudithk@gmail.com>
2022-04-07 10:48:04 -0500

    maquetado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOMClient from 'react-dom/client';
+import AppRouters from './routers/AppRouters';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderizar = document.getElementById('root')
+ReactDOMClient.createRoot(renderizar).render(
+ <AppRouters/>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
